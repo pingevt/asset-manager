c1a39f3aaf27e0eb80bd5d22abe865fc45870044
Pete Inge <pete.inge@gmail.com>
2016-01-24 18:27:34 -0500

    assets/conditions devel
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

---
+++ sites/all/modules/custom/asset_tool/modules/asset/templates/asset.tpl.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<div id="asset-<?php print $asset->aid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $asset_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <label>Type:</lable>
+    <div><?php print $asset->type; ?></div>
+    <label>Description:</lable>
+    <div><?php print check_markup($asset->description, $asset->description_format); ?></div>
+    <label>Starting Year:</lable>
+    <div><?php print $asset->start_period_year; ?></div>
+    <label>Starting Period:</lable>
+    <div><?php print $asset->start_period; ?></div>
+    <?php
+      print render($content);
+    ?>
+  </div>
+</div>
